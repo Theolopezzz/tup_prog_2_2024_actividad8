6217a57a4976591939f830b2edf1d4d7a376fc03
theolopezzz <lopeztheo31245@gmail.com>
2024-10-17 17:38:19 -0300

    en proceso 1

---
+++ Ej 8.1/Models/Cuenta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_8._1.Models
+{
+    internal class Cuenta
+    {
+    }
+}
